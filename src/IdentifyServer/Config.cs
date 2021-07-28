1edef2407f35cb210cd58d58ef06b4abec12997a
Cleidson Oliveira <dev.cleidson@gmail.com>
2021-07-28 21:33:00 +0000

    feat: added Api and Client

--- src/IdentifyServer/Config.cs
+++ src/IdentifyServer/Config.cs
@@ -7,20 +7,29 @@ using System.Collections.Generic;
 
 namespace IdentifyServer
 {
-    public static class Config
-    {
-        public static IEnumerable<IdentityResource> IdentityResources =>
-            new IdentityResource[]
-            { 
+  public static class Config
+  {
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
                 new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>{
+                new ApiScope("api1", "My Api")
             };
 
-        public static IEnumerable<ApiScope> ApiScopes =>
-            new ApiScope[]
-            { };
-
-        public static IEnumerable<Client> Clients =>
-            new Client[] 
-            { };
-    }
+    public static IEnumerable<Client> Clients =>
+        new List<Client>{
+            new Client{
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = {
+                    new Secret("secret".Sha256())
+                },
+                AllowedScopes = {"api1"}
+            }
+        };
+  }
 }
